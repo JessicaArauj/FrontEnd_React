bdf87ab72b7cefb383fcfff83498d0aae659fa9c
Jessica Teixeira Araujo <jessicateixeiraaraujo@gmail.com>
2023-07-17 12:15:08 -0300

    feat:creat projet React

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # FrontEnd_React
+
+npm creat vite
+
+npm i vite
+
+npm run dev
+
+https://www.npmjs.com/package/vite
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div>
+      Item card
+    </div>
+    </>
+  )
+}
+
+export default App
